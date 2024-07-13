db655766f043efab0fde346996430818648f2fbc
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-07-13 19:35:51 +0200

    issue

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a12008d101f0da4b7687bab441f3a0a50c7046c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f66bb7cf2a18b4cef11b90fa6f30679fbfc87be4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
