8197a45394fba34d6b0e53bb7930dcd48d0ff51b
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-07-15 21:51:46 +0200

    added client app

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7e58b51fa43dc67e00c2c8f74389afdda60798a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31392f22d3a56ba0205b087f1917862f0c4659bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
