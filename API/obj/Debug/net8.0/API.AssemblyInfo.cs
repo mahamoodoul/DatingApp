d336d7b9772ef334c562eb16b4556a40584c1e36
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-07-25 21:38:16 +0200

    added middleware and error code

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b97d423970922c50f1aac0932b0e996f2b24a68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9e0c5d5c67646daa7a87a49295526b551875fe8")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
