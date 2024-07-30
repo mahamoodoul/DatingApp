128b73b8497719a96595e40735030a51f5d4ede4
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-07-31 00:06:05 +0200

    added photo add and set main

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+776aa634a06683f6cd9a3f51d29b82565b4b04b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+904fed3b9a58e94e43e471cc00fa4cd759d3aaa3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
