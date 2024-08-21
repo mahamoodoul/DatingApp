e1ab1f13c01ba8eddb2718309fc8f3b75114c7ce
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-08-21 23:49:59 +0200

    added pagination extension

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ce59c07890a8c1e362c70bbe861809747b98d81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7a96ed1dcd77e3d42ea6da9e126df14a91114c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
