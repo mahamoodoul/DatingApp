88ffa47dcfad04993467312bcce579ee23412351
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-08-23 14:02:59 +0200

    updated user dto in controller

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -38,7 +38,8 @@ public class AccountController(DataContext context, ITokenService tokenService,
         {
             Username = user.UserName,
             Token = tokenService.CreateToken(user),
-            KnownAs = user.KnownAs
+            KnownAs = user.KnownAs,
+            Gender = user.Gender
         };
     }
 
@@ -65,6 +66,7 @@ public class AccountController(DataContext context, ITokenService tokenService,
         {
             Username = user.UserName,
             KnownAs = user.KnownAs,
+            Gender = user.Gender,
             Token = tokenService.CreateToken(user),
             PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
         };
