c3416e89f1377d8955f24c034b3f06fa9df0e6bb
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-08-26 11:42:48 +0200

    added like controller

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+112032578241bb485d1d2d3b4f128dfe0fe158d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6335f9b095a4159ef81614ee5dd61900328056b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
