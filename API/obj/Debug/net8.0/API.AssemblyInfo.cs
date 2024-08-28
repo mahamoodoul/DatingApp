a4e90646872c74a4045725667e2b6f8991d6df2b
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-08-28 16:54:54 +0200

    setting up message database schema and add data context

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03d11503502802f08eccbf511dab60a83bab6e2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb35dc621c14da1515020286239f68916588f7a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
