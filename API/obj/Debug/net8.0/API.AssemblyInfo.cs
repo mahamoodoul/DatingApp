ec2a5ed4da8bee75ed8d15d997216b8bd7dd7f9b
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-09-04 20:28:19 +0200

    updated app routes

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44bf61bd2bbf146a7d7dd91628382cd7c8dc3550")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ed7b9fc72630dee3b8e9526e2d35da249f1cda6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
