a7123796c8935bdef1f90194d1db8fde7b42eed1
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-09-07 21:19:50 +0200

    member details update

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ed7b9fc72630dee3b8e9526e2d35da249f1cda6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bae83a7499a6eefae95693734fd7047d0d2d62a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
